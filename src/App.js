3a831eb7992a6b09eee51bbff77dd4daff292a47
ctrubey3 <70168073+ctrubey3@users.noreply.github.com>
2021-04-01 11:54:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import {data} from "./components/data";
+import { v4 as uuidv1 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import All from "./Pages/All";
+import Animation from "./Pages/Animation";
+import Games from "./Pages/Games";
+import Television from "./Pages/Television";
+import UserInfo from "./Pages/UserInfo";
+
+
+
+import Box from "./components/Box";
+import Nav from "./components/Nav";
+
+class App extends Component {
+  state = {
+    data: data,
+    name: '',
+    category: '',
+    value: '',
+    id: uuidv1(),
+    img: '',
+    search: '',
+
+  }
+  getFilterData = () => {
+    return this.state.data.filter(category =>
+        category.name.toLowerCase().includes(this.state.search.toLowerCase())
+    )
+  }
+
+
+
+
+
+  render() {
+    return (
+        <Router>
+          <div className="App">
+            <Nav/>
+            <Switch>
+              <Route exact path={"/"} component={All}/>
+              <Route exact path={"/Animation"} component={Animation}/>
+              <Route exact path={"/Games"} component={Games}/>
+              <Route exact path={"/Television"} component={Television}/>
+              <Route exact path={"/UserInfo/:id"} component={UserInfo}/>
+              <Box getFilterData={this.getFilterData}/>
+            </Switch>
+          </div>
+
+        </Router>
+    );
+  }
 }
 
 export default App;
+
